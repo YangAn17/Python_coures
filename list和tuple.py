96f6b006c198f8fe643acffa119cd71f4466e925
YangAn <17680511134@163.com>
2024-03-15 16:09:03 +0800

    first commit

---
+++ list和tuple.py
@@ -0,0 +1,28 @@
+## list是一种有序的集合，可以随时添加和删除其中的元素
+#
+classmates = ["Michael", "Bob", "Tracy"]
+print(classmates)
+print(len(classmates))  # len() 函数可以获得list元素的个数
+classmates.append("Adam")  # append() 方法可以追加元素到list末尾
+print(classmates)
+classmates.insert(1, "Jack")  # insert() 方法可以把元素插入到指定的位置
+print(classmates)
+classmates.pop()  # pop() 方法可以删除list末尾的元素
+print(classmates)
+classmates.pop(1)  # pop(i) 方法可以删除list指定位置的元素
+print(classmates)
+classmates[1] = "Sarah"  # list中的元素也可以直接赋值
+print(classmates)
+
+## tuple是一种有序的集合，一旦初始化就不能修改
+# tuple所谓的不变是说指向的元素不变，即指向'a'，就不能改成指向'b'，但指向的'a'本身是可变的
+#
+t = 1
+print(t)  # 这是一个数
+t = (1,)
+print(t)  # 这是一个tuple
+
+t = ("a", "b", ["A", "B"])
+t[2][0] = "X"
+t[2][1] = "Y"
+print(t)  # tuple不变性的体现
---
+++ str格式化.py
@@ -0,0 +1,19 @@
+## 使用 % 运算符
+# %s 字符串 %d 整数 %f 浮点数 %x 十六进制整数
+#
+print("Hello, %s" % "world")
+print("Hi, %s, you have $%d." % ("Michael", 1000000))
+print("%2d-%02d" % (3, 1))  # 格式化整数和浮点数还可以指定是否补0和整数与小数的位数
+print("%.2f" % 3.1415926)
+
+## 使用 format() 方法
+# 用{}和:代替%，后面用format()方法传入参数
+#
+print("Hello, {0}, 成绩提升了 {1:.1f}%".format("小明", 17.125))
+
+## 使用 f-string
+# 在字符串前面加上f或F，直接把变量放入大括号{}中
+#
+r = 2.5
+s = 3.14 * r**2
+print(f"The area of a circle with radius {r} is {s:.2f}")
